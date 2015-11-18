0d71049c60ac26bae95701486e7385a33621dc4e
vadimvoyevoda <vadimvoyevoda@gmail.com>
2015-11-18 18:58:51 +0200

    Add About Page

---
+++ General_EvetsParty_MVC/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace General_EvetsParty_MVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
